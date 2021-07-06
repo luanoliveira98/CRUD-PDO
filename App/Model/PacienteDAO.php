0ae7788e51e11c9eb03ad1a310170bee21fca1ed
Luan Oliveira <luanoliveira17@hotmail.com>
2021-07-06 15:16:54 -0300

    Create crud functions for Paciente

---
+++ App/Model/PacienteDAO.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Model;
+
+class PacienteDAO {
+
+    public function create(Paciente $p) {
+
+    }
+
+    public function read() {
+
+    }
+
+    public function update(Paciente $p) {
+
+    }
+
+    public function delete(int $id) {
+
+    }
+}
