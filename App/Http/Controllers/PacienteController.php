526b7e333e5faf36eb3521ac93dd34b17443e354
Luan Oliveira <luanoliveira17@hotmail.com>
2021-07-07 00:33:46 -0300

    Adicionando metodos de recurso na controller de paciente

--- App/Http/Controllers/PacienteController.php
+++ App/Http/Controllers/PacienteController.php
@@ -3,6 +3,41 @@
 
 namespace App\Controllers;
 
+use App\Model\Paciente;
 class PacienteController extends Controller {
 
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store()
+    {
+        
+    }
+
+    public function show(int $id)
+    {
+        
+    }
+
+    public function edit(int $id)
+    {
+        
+    }
+
+    public function update(int $id)
+    {
+        
+    }
+
+    public function destroy(int $id)
+    {
+        
+    }
 }
