9ef2b392a707c23762daab996baccd8d896e3f14
Luan Oliveira <luanoliveira17@hotmail.com>
2021-07-07 22:15:00 -0300

    Rotas de consulta

--- Routes/web.php
+++ Routes/web.php
@@ -31,6 +31,16 @@ $router->get("/{id}", "PacienteController:show");
 $router->put("/{id}", "PacienteController:update");
 $router->delete("/{id}", "PacienteController:destroy");
 
+/**
+ * Consulta - API
+ */
+$router->group("consultas");
+$router->get("/", "ConsultaController:index");
+$router->post("/", "ConsultaController:store");
+$router->get("/{id}", "ConsultaController:show");
+$router->put("/{id}", "ConsultaController:update");
+$router->delete("/{id}", "ConsultaController:destroy");
+
 $router->dispatch();
 
 if($router->error()) {
